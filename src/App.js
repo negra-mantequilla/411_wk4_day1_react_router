d73812c4af446235ed06f842a0de008ba746a1c6
Joe Perry <japerry16633@gmail.com>
2022-03-21 15:13:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
+import React from "react";
+import Navigation from "./components/Navigation";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 // Write imports for Router & BrowserRouter here //
+import Car from "./components/Car";
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation />
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/car/:id" component={Car} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import cars from '../cars.json'
+import React from "react";
+import cars from "../cars.json";
+// read the props.match.params.id (how to filter an array)
+// need to filter the array based on the id you are getting 
+// id found in the object
+// display the car info usin Material chips 
+
+
 // import material ui components here //
 // Container, Paper, Chip //
+// import {useParams} from "react-router-dom"
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  return <h1>A specific car</h1>;
+};
 
-export default Car
+export default Car;
